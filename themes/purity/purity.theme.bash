54089787cb31c29020a58efe9823e2e382aad9df
BarbUk <julien.virey@gmail.com>
2017-01-04 00:49:11 +0400

    custom purity theme for server

--- themes/purity/purity.theme.bash
+++ themes/purity/purity.theme.bash
@@ -1,21 +1,36 @@
 #!/usr/bin/env bash
 
-SCM_THEME_PROMPT_DIRTY=" ${bold_red}⊘${normal}"
-SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓${normal}"
-SCM_THEME_PROMPT_PREFIX="${reset_color}( "
-SCM_THEME_PROMPT_SUFFIX=" ${reset_color})"
-
-GIT_THEME_PROMPT_DIRTY=" ${bold_red}⊘${normal}"
-GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓${normal}"
-GIT_THEME_PROMPT_PREFIX="${reset_color}( "
-GIT_THEME_PROMPT_SUFFIX=" ${reset_color})"
+SCM_GIT_CHAR="± "
+SCM_HG_CHAR="☿ "
+SCM_SVN_CHAR="⑆ "
+SCM_NONE_CHAR=""
+SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+SCM_THEME_PROMPT_PREFIX="|"
+SCM_THEME_PROMPT_SUFFIX="${green}| "
+SCM_GIT_BEHIND_CHAR="${red}↓${normal}"
+SCM_GIT_AHEAD_CHAR="${bold_green}↑${normal}"
+SCM_GIT_UNTRACKED_CHAR="⌀"
+SCM_GIT_UNSTAGED_CHAR="${bold_yellow}•${normal}"
+SCM_GIT_STAGED_CHAR="${bold_green}+${normal}"
+GIT_THEME_PROMPT_DIRTY=" ${bold_red}✗"
+GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+GIT_THEME_PROMPT_PREFIX="${cyan}"
+GIT_THEME_PROMPT_SUFFIX="${cyan}"
+SCM_THEME_BRANCH_TRACK_PREFIX=' ⤏  '
+icon_branch="🌿"
 
 STATUS_THEME_PROMPT_BAD="${bold_red}❯${reset_color}${normal} "
 STATUS_THEME_PROMPT_OK="${bold_green}❯${reset_color}${normal} "
 
+function git_prompt_info {
+  git_prompt_vars
+  echo -e "on $icon_branch $SCM_PREFIX$SCM_BRANCH$SCM_STATE$SCM_GIT_AHEAD$SCM_GIT_BEHIND$SCM_GIT_STASH$SCM_SUFFIX"
+}
+
 function prompt_command() {
     local ret_status="$( [ $? -eq 0 ] && echo -e "$STATUS_THEME_PROMPT_OK" || echo -e "$STATUS_THEME_PROMPT_BAD")"
-    PS1="\n${blue}\u \w $(scm_prompt_info)\n${ret_status} "
+    PS1="\n${purple}\u${bold_blue} in ${blue}\w $(scm_prompt_info)\n${ret_status}"
 }
 
 safe_append_prompt_command prompt_command
